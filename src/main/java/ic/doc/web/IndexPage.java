f571bb75f1587029dff27670644ecefab78d83d7
Robert Chatley <robert@develogical.com>
2014-12-27 18:12:00 +0000

    small text change

--- src/main/java/ic/doc/web/IndexPage.java
+++ src/main/java/ic/doc/web/IndexPage.java
@@ -8,7 +8,7 @@ public class IndexPage extends HtmlPage {
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
                 "<h1>Welcome!</h1>" +
-                "<p>Enter your query in the box: " +
+                "<p>Type your query in the box: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
                   "</form>" +
