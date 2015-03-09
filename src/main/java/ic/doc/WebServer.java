3edf88d8e3dc68f23403f7689f1532b95b8247f4
Varun Verma <varun.verma11@imperial.ac.uk>
2015-03-09 10:12:42 +0000

    largest number done

--- src/main/java/ic/doc/WebServer.java
+++ src/main/java/ic/doc/WebServer.java
@@ -3,19 +3,23 @@ package ic.doc;
 import ic.doc.web.ApiResponse;
 import ic.doc.web.IndexPage;
 import ic.doc.web.ResultsPage;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-public class WebServer {
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
-    public WebServer() throws Exception {
+public class WebServer
+{
+
+    public WebServer() throws Exception
+    {
         Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
@@ -26,26 +30,49 @@ public class WebServer {
         server.join();
     }
 
-    static class Website extends HttpServlet {
+    static class Website extends HttpServlet
+    {
         @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            if (req.getParameter("q") == null) {
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+        {
+            String qParam = req.getParameter("q");
+            if (qParam == null)
+            {
                 new IndexPage().writeTo(resp);
-            } else {
-                new ResultsPage(req.getParameter("q")).writeTo(resp);
+            } else if (qParam.startsWith("which of the following numbers is the largest"))
+            {
+                String[] numbers = qParam.split(":")[1].split(",");
+                new ResultsPage(findMaxInt(numbers) + "").writeTo(resp);
+            } else
+            {
+                new ResultsPage(qParam).writeTo(resp);
             }
+        }
+
+        private int findMaxInt(String[] list)
+        {
+            int result = Integer.MIN_VALUE;
+            for (String i : list)
+            {
+                int num = Integer.parseInt(i);
+                if (result < num)
+                    result = num;
+            }
+            return result;
         }
     }
 
-    static class Api extends HttpServlet {
+    static class Api extends HttpServlet
+    {
         @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+        {
             new ApiResponse(req.getParameter("q")).writeTo(resp);
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception
+    {
         new WebServer();
     }
 }
-
