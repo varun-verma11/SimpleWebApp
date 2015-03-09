8517fdba3e228e3b1012c0c504d0e2e27e4395b0
Varun Verma <varun.verma11@imperial.ac.uk>
2015-03-09 09:35:59 +0000

    chaning text

--- src/main/java/ic/doc/web/IndexPage.java
+++ src/main/java/ic/doc/web/IndexPage.java
@@ -2,17 +2,14 @@ package ic.doc.web;
 
 import java.io.PrintWriter;
 
-public class IndexPage extends HtmlPage {
+public class IndexPage extends HtmlPage
+{
 
     @Override
-    protected void writeContentTo(PrintWriter writer) {
-        writer.println(
-                "<h1>Welcome!</h1>" +
-                "<p>Enter your query in the box below: " +
-                  "<form><input type=\"text\" name=\"q\" />" +
-                    "<input type=\"submit\">" +
-                  "</form>" +
-                "</p>");
+    protected void writeContentTo(PrintWriter writer)
+    {
+        writer.println("<h1>Welcome!</h1>" + "<p>Please enter your query in the box below: " + "<form><input type=\"text\" name=\"q\" />"
+                + "<input type=\"submit\">" + "</form>" + "</p>");
     }
-    
+
 }
